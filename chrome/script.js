2ff0e50f32b6957151b093d0646e23929be3eed7
Kevin Carmody <kevin@skinofstars.com>
2015-05-11 19:29:20 +0100

    update web copy. repack app with analytics

--- chrome/script.js
+++ chrome/script.js
@@ -105,3 +105,15 @@ function listItemInHaystack(list, haystack) {
   }
   return isMatch;
 }
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-12736204-7']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = 'https://ssl.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+
