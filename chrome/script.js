6f87c6c3de05f52aa126efdac9907adc7cde557a
Kevin Carmody <kevin@skinofstars.com>
2015-05-13 23:34:48 +0100

    js in to safer function

--- chrome/script.js
+++ chrome/script.js
@@ -1,118 +1,123 @@
 
+(function(){
 
-getOutlet(function($story) {
-  applyCTAs($story);
-});
-
-
-function getOutlet(callback) {
-  var hostname = window.location.hostname
-  var $story
-
-  if ( hostname.indexOf('bbc') != -1 ) {
-    $story = $('.story-body');
-  }
-
-  if ( hostname.indexOf('guardian') != -1 ) {
-    var $story = $('.content__article-body');
-  }
-
-  if ( hostname.indexOf('dailymail') != -1 ) {
-    var $story = $('.article-text');
-  }
-
-  if ( hostname.indexOf('telegraph') != -1 ) {
-    var $story = $('.story');
-  }
-
-  if ( hostname.indexOf('huffingtonpost') != -1 ) {
-    var $story = $('#mainentrycontent');
-  }
-
-  if ( hostname.indexOf('forbes') != -1 ) {
-    var $story = $('.article_body.article');
-  }
-
-  callback($story)
-}
+  getOutlet(function($story) {
+    applyCTAs($story);
+  });
 
 
-function applyCTAs($story) {
-  var $container = $('<div id="fl-cta-container"></div>');
+  function getOutlet(callback) {
+    var hostname = window.location.hostname
+    var $story
 
-  // only do something if we have a $story
-  if ($story != undefined) {
+    if ( hostname.indexOf('bbc') != -1 ) {
+      $story = $('.story-body');
+    }
 
-    getActions($story, function(matches) {
-      for (var i = matches.length - 1; i >= 0; i--) {
-        $container.append('<a class="cta-help-button cta-button" href="'+matches[i].url+'">'+matches[i].title+'</a>')
-      };
+    if ( hostname.indexOf('guardian') != -1 ) {
+      var $story = $('.content__article-body');
+    }
 
-      $container.append('<div class="cta-about"><a href="http://call-to-action.skinofstars.com/">Suggest an action</a> &middot; <a href="http://call-to-action.skinofstars.com/">About Call To Action</a></div>');
+    if ( hostname.indexOf('dailymail') != -1 ) {
+      var $story = $('.article-text');
+    }
 
-      $story.append($container)
-    });
+    if ( hostname.indexOf('telegraph') != -1 ) {
+      var $story = $('.story');
+    }
+
+    if ( hostname.indexOf('huffingtonpost') != -1 ) {
+      var $story = $('#mainentrycontent');
+    }
+
+    if ( hostname.indexOf('forbes') != -1 ) {
+      var $story = $('.article_body.article');
+    }
+
+    callback($story)
   }
 
 
-}
+  function applyCTAs($story) {
+    var $container = $('<div id="fl-cta-container"></div>');
+
+    // only do something if we have a $story
+    if ($story != undefined) {
+
+      getActions($story, function(matches) {
+        for (var i = matches.length - 1; i >= 0; i--) {
+          $container.append('<a class="cta-help-button cta-button" href="'+matches[i].url+'">'+matches[i].title+'</a>')
+        };
+
+        $container.append('<div class="cta-about"><a href="http://call-to-action.skinofstars.com/">Suggest an action</a> &middot; <a href="http://call-to-action.skinofstars.com/">About Call To Action</a></div>');
+
+        $story.append($container)
+      });
+    }
 
 
-function getActions($story, callback) {
+  }
 
-  // As a temporary solution, we'll just return all the available ctas and sort
-  // out what we want here. This is just a demo after all.
 
-  // Alternative solutions:
-  // - submit text of story
-  // - submit URL of story
+  function getActions($story, callback) {
 
-  $.getJSON('//call-to-action.skinofstars.com/ctas.json')
-    .done(function(ctaActions) {
-      var matches = [];
+    // As a temporary solution, we'll just return all the available ctas and sort
+    // out what we want here. This is just a demo after all.
 
-      // iterate actions
-      for ( var i = 0; i < ctaActions.length; i++ ) {
-        var match = false;
+    // Alternative solutions:
+    // - submit text of story
+    // - submit URL of story
 
-        var storyText = $story.text().toLowerCase();
+    $.getJSON('//call-to-action.skinofstars.com/ctas.json')
+      .done(function(ctaActions) {
+        var matches = [];
 
-        // is there an action always word?
-        if ( ctaActions[i].always != undefined ) {
-          // does the story have the always word?
-          if ( storyText.indexOf(ctaActions[i].always.toLowerCase()) != -1 ) {
+        // iterate actions
+        for ( var i = 0; i < ctaActions.length; i++ ) {
+          var match = false;
 
-            // still need a secondary keyword
+          var storyText = $story.text().toLowerCase();
+
+          // is there an action always word?
+          if ( ctaActions[i].always != undefined ) {
+            // does the story have the always word?
+            if ( storyText.indexOf(ctaActions[i].always.toLowerCase()) != -1 ) {
+
+              // still need a secondary keyword
+              if ( listItemInHaystack(ctaActions[i].keywords, storyText) ) {
+                matches.push(ctaActions[i]);
+              }
+            }
+          }
+          // no always word? just do general keyword search then
+          else {
             if ( listItemInHaystack(ctaActions[i].keywords, storyText) ) {
               matches.push(ctaActions[i]);
             }
           }
         }
-        // no always word? just do general keyword search then
-        else {
-          if ( listItemInHaystack(ctaActions[i].keywords, storyText) ) {
-            matches.push(ctaActions[i]);
-          }
-        }
-      }
 
-      callback(matches)
+        callback(matches)
 
-    });
+      });
 
-}
-
-
-function listItemInHaystack(list, haystack) {
-  var isMatch = false;
-  // iterate actions' keywords
-  for ( var j = 0; j < list.length; j++ ) {
-    if ( haystack.indexOf(list[j].toLowerCase()) != -1 ) {
-      isMatch = true
-    }
   }
-  return isMatch;
-}
+
+
+  function listItemInHaystack(list, haystack) {
+    var isMatch = false;
+    // iterate actions' keywords
+    for ( var j = 0; j < list.length; j++ ) {
+      if ( haystack.indexOf(list[j].toLowerCase()) != -1 ) {
+        isMatch = true
+      }
+    }
+    return isMatch;
+  }
+
+
+})();
+
 
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-12736204-7']);
